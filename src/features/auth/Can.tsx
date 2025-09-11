16bcf84e7e15bc687885250433cfe910fe8ce88b
Flowzym <hypophobia@gmail.com>
2025-09-11 19:39:37 +0200

    Delete src directory

--- src/features/auth/Can.tsx
+++
@@ -1,4 +0,0 @@
-export * from '../../components/auth/Can';
-export {} from '../../components/auth/Can';
-export * from '../../components/auth/Can';
-export {} from '../../components/auth/Can';
--- src/utils/cn.ts
+++
@@ -1,6 +0,0 @@
-/**
- * Utility für Tailwind-Klassennamen-Zusammenführung
- */
-export function cn(...classes: (string | undefined | null | false)[]): string {
-  return classes.filter(Boolean).join(' ');
-}
