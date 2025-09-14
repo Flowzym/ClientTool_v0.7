7a052421f4c0518e15d7b7f1ea95159a1378cedb
Flowzym <hypophobia@gmail.com>
2025-09-14 11:45:41 +0200

    Fix ContactDialog import path in components index

--- src/features/board/components/index.ts
+++ src/features/board/components/index.ts
@@ -7,7 +7,7 @@ export { default as ClientRow } from './ClientRow';
 export { default as BatchActionsBar } from './BatchActionsBar';
 export { default as BoardHeader } from './BoardHeader';
 export { default as ExportDialog } from '../ExportDialog';
-export { default as ContactDialog } from './ContactDialog';
+export { default as ContactDialog } from '../ContactDialog';
 export { default as ClientInfoDialog } from './ClientInfoDialog';
 export { default as ExportCsvDialog } from './ExportCsvDialog';
 export { default as VirtualizedBoardList } from './VirtualizedBoardList';
