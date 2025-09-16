8d5db6de4f1bf9bf0d679e4a93487040e1c8e57c
Flowzym <hypophobia@gmail.com>
2025-09-16 19:53:35 +0200

    Consolidate useBoardData and fix overlay import paths

--- src/features/board/hooks/index.ts
+++ src/features/board/hooks/index.ts
@@ -2,7 +2,3 @@
  * Board hooks barrel
  * All hooks exported as named
  */
-
-export { useBoardActions } from './useBoardActions';
-export { useBoardData } from './useBoardData';
-export { useOptimisticOverlay } from './useOptimisticOverlay';
